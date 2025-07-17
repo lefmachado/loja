66879bb2b8bbdb853499c9bcbd2444807f884401
Leandro Machado <lefmachado@gmail.com>
2025-07-17 20:44:28 -0300

    Add logging and CRUD operations for Produto and Categoria entities

--- src/main/java/br/com/estudo/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/estudo/loja/dao/ProdutoDao.java
@@ -3,6 +3,7 @@ package br.com.estudo.loja.dao;
 import br.com.estudo.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ProdutoDao {
 
@@ -15,4 +16,23 @@ public class ProdutoDao {
     public void cadastrar(Produto produto) {
         this.em.persist(produto);
     }
+
+    public void atualizar(Produto produto) {
+        this.em.merge(produto);
+    }
+
+    public void remover(Produto produto) {
+        produto = this.em.merge(produto);
+        this.em.remove(produto);
+    }
+
+    public Produto burcarPorId(Long id) {
+        return this.em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return this.em.createQuery(jpql, Produto.class).getResultList();
+    }
 }
+
--- src/main/java/br/com/estudo/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/estudo/loja/testes/CadastroDeProduto.java
@@ -8,10 +8,28 @@ import br.com.estudo.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CadastroDeProduto {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CadastroDeProduto.class);
+
     public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.burcarPorId(1L);
+        LOGGER.info("Preço do produto encontrado: {}", p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarTodos();
+        todos.forEach(produto -> LOGGER.info("Produto: {}", produto.getNome()));
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
         Produto celular = new Produto("Xiaomi Redmi",
                 "Muito legal",
